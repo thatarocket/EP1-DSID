2620a0c3980d805afc0522309f1fa4d69c5792a9
thatarocket <thais.souza.rodrigues20722@gmail.com>
2023-05-19 14:22:23 -0300

    fixing server methods

--- Global/IPartRepository.java
+++ Global/IPartRepository.java
@@ -5,12 +5,7 @@ import java.rmi.RemoteException;
 import java.util.List;
 
 public interface IPartRepository extends Remote {
-    public boolean funcionando() throws RemoteException;
-
     public void addP(Part part) throws RemoteException;
     public Part getP(int codigo) throws RemoteException;
-    public List<Part> listP() throws Exception; 
-    public List<Part> clearList(List<Part> subParts) throws RemoteException;
-    public void addSubPart(List<Part> subParts, Part part) throws RemoteException; 
-    public void showPart(Part part) throws RemoteException; //showp
+    public List<Part> listP() throws Exception;  
 }
--- Global/PartRepository.java
+++ Global/PartRepository.java
@@ -12,10 +12,6 @@ public class PartRepository extends UnicastRemoteObject implements IPartReposito
         super();
     }
 
-    public boolean funcionando() throws RemoteException {
-        return true;
-    }
-
     @Override
     public void addP(Part part) throws RemoteException {
         partRep.add(part);
@@ -23,7 +19,6 @@ public class PartRepository extends UnicastRemoteObject implements IPartReposito
 
     @Override
     public Part getP(int codigo) throws RemoteException {
-        System.out.println("Entrei no getP");
         if(partRep.isEmpty()) return null;
         for (Part part : partRep) {
             if (part.getCodigo() == codigo) return part;  
@@ -35,25 +30,5 @@ public class PartRepository extends UnicastRemoteObject implements IPartReposito
     public List<Part> listP() throws Exception {
         if (partRep.size() == 0) return null;
         return partRep;
-    }
-
-    @Override
-    public List<Part> clearList(List<Part> subParts) throws RemoteException {
-       subParts.clear();
-       return subParts;
-    }
-
-    @Override
-    public void addSubPart(List<Part> subParts, Part part) throws RemoteException {
-        subParts.add(part);
-    }
-
-    @Override
-    public void showPart(Part part) throws RemoteException {
-        System.out.println("Código part: " + part.getCodigo());
-        System.out.println("Nome part: " + part.getNome());
-        System.out.println("Descrição part: " + part.getDescricao());
-        System.out.println("Subparts: " + part.getSubparts());
-        System.out.println("--------------------------------------------------");
     }
 }
